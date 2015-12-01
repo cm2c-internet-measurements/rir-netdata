31e4ccc05f7aeb5d57a9323499134b084faf8c89
Carlos M. Martinez <cmm@cagnazzo.me>
2015-12-01 18:33:56 -0300

    added delegated library

---
+++ lib/lacniclabs/delegated/delegated.py
@@ -0,0 +1,13 @@
+#########################################################################################
+'''
+delegated.py: Implements delegated API on top of csv import
+(c) carlos@lacnic.net 20151201
+'''
+
+from cm2c.csvimport import sql3load
+
+class delegatedStats(self):
+    pass
+
+# END file
+#########################################################################################
