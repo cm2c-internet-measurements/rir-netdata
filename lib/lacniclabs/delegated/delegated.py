f6018740eb5cae44684b58e72aeeeed410345e1f
Carlos M. Martinez <carlos@lacnic.net>
2015-12-21 14:15:48 -0300

    delegated now imports files correctly and adds prefix column

--- lib/lacniclabs/delegated/delegated.py
+++ lib/lacniclabs/delegated/delegated.py
@@ -9,33 +9,52 @@ from cm2c.commons.gen.utils import get_tmp_fn
 from cm2c.commons.gen.getfile import getfile
 from cm2c.commons.debug.dprint import dprint
 from lacniclabs.etc import rirconfig
+import math
+import ipaddr
 
 ## begin
 class delegatedStats(object):
     '''
     Imports delegated stats file from the RIRs.
+
+    Class attributes:
+        s3l / dbh : handle to sql3load instance.
     '''
 
-    dbh = 0
-    '''
-    dbh: sql3load database handle.
-    '''
+    #dbh = None
+    #'''
+    #dbh: sql3load database handle.
+    #'''
 
     # begin
     def __init__(self, **kwargs):
+        """
+        Class constructor, receives keyword arguments {rir} and {date}
+        """
         self.drir  = kwargs.get('rir','lacnic')
         self.ddate = kwargs.get('date', 'latest')
+        self.local_file = kwargs.get('local_file', None)
+
         # get archivo delegated
-        self._download_stats_file(rir=self.drir, date=self.ddate)
+        if self.local_file == None:
+            self.dlg_fn_name = self._download_stats_file(rir=self.drir, date=self.ddate)
+        else:
+            self.dlg_fn_name = self.local_file
 
         ## load into database
-        self.s3_template = [ ('name', 'text'), ('age', 'integer'), ('weigth', 'float') ]
-        self.s3l = sql3load(self.s3_template, get_tmp_fn(".db") )
-        r = self.s3l.importFile("test/test-import.txt")
-        self.dbh = sql3load("")
+        self.s3_template = [ ('rir', 'text'), ('cc', 'text'), ('type', 'text'),
+                             ('block', 'text'), ('length', 'integer'), ('date', 'integer'),
+                             ('status', 'text'), ('orgid', 'integer')
+                            ]
+        self.s3l = sql3load(self.s3_template, get_tmp_fn(".db"), "numres" )
+        # print self.dlg_fn_name
+        r = self.s3l.importFile(self.dlg_fn_name, "|")
+        # self.dbh = sql3load("")
+        self.dbh = self.s3l
         pass
     # end
 
+    # begin
     def _download_stats_file(self, **kwargs):
         """
         Downloads stats file for a given rir and date.
@@ -47,9 +66,57 @@ class delegatedStats(object):
         ddate = kwargs.get('date', 'latest')
         drir  = kwargs.get('rir', 'lacnic')
         dp.log("Downloading stat file for RIR %s, date %s..." % (self.drir, self.ddate))
-        dlg_tmpfile = get_tmp_fn("delegated-extended-%s-%s" % (drir, ddate))
-        dlg_tmpfile = getfile(rirconfig.rir_config_data[self.drir]['dlge'][0] % (self.ddate), dlg_tmpfile,43200)
-        pp.log(" OK\n")
+        dlg_tmpfile = get_tmp_fn(filename="delegated-extended-%s-%s" % (drir, ddate) )
+        dlg_tmpfile_name = getfile(rirconfig.rir_config_data[self.drir]['dlge'][0] % (self.ddate), dlg_tmpfile,43200)
+        dp.log(" OK\n")
+        return dlg_tmpfile_name
+    # end
+
+    # begin
+    def _add_numeric_columns(self):
+        r1 = self.dbh.addMetaColumn("istart INTEGER")
+        r2 = self.dbh.addMetaColumn("iend INTEGER")
+        return r1 and r2
+    #end
+
+    # begin
+    def _add_prefix_column(self):
+        r = self.dbh.addMetaColumn("prefix VARCHAR(80)")
+        p = self.dbh.calculateMetaColumn("prefix", self._populate_prefix_column)
+        return r
+    # end
+    #begin
+    #@staticmethod
+    def _populate_prefix_column(self,w_row):
+        if w_row['type'] == 'ipv4' and w_row['block'] != '*':
+            pfx_len = 32 - int( math.log( int(w_row['length']), 2) )
+            pfx = ipaddr.IPv4Network(w_row['block'] + "/" + str(pfx_len))
+            #w_row['prefix'] = str(pfx)
+            #w_row['istart'] = int(pfx.network)
+            #w_row['iend'] = int(pfx.broadcast)
+            #w_row['equiv'] = (w_row['iend']-w_row['istart'])/256 + 1
+            return str(pfx)
+        elif w_row['type'] == 'ipv6' and w_row['block'] != '*':
+            pfx_norm_base = pow(2,64)
+            #print "row %s" % (dict(w_row))
+            pfx = ipaddr.IPv6Network( w_row['block'] + "/" +  str(w_row['length']) )
+            #w_row['prefix'] = str(pfx)
+            #w_row['istart'] = int(pfx.network) / pfx_norm_base
+            #w_row['iend'] = int(pfx.broadcast) / pfx_norm_base
+            #w_row['equiv'] = (w_row['iend'] - w_row['istart'] + 1) / pow(2,32)
+            return str(pfx)
+        else:
+            #w_row['prefix'] = 'na/asn'
+            #w_row['istart'] = int(w_row['start'])
+            #w_row['iend'] = int(w_row['start']) + int(w_row['value'])
+            #w_row['equiv'] = 'na'
+            return 'na/asn'
+    #end
+
+    # begin
+    def _add_equiv_column(self):
+        r = self.dbh.addMetaColumn("equiv INTEGER")
+        return r
     # end
 
 ## end
--- lib/lacniclabs/test/test_DelegatedStats.py
+++ lib/lacniclabs/test/test_DelegatedStats.py
@@ -16,8 +16,15 @@ from lacniclabs.delegated.delegated import delegatedStats
 #--
 class Test(unittest.TestCase):
 
+    _setuponce_done = False
+    dsreader = None
+
     def setUp(self):
-        self.dsreader = delegatedStats(rir="lacnic", date="latest")
+        if not self._setuponce_done:
+            self.__class__.dsreader = delegatedStats(local_file="tmp/delegated-extended-lacnic-short")
+            self.__class__._setuponce_done = True
+        else:
+            pass
     ## end
 
     def tearDown(self):
@@ -27,6 +34,53 @@ class Test(unittest.TestCase):
     def testClassInstantiation(self):
         # test relies on logic put in the setUp method
         pass
+    ## end
+
+    def testQueryDelegated1(self):
+        rs1 = self.__class__.dsreader.dbh._rawQuery("SELECT count(*) as cnt FROM numres")
+        # rs1 = self.dsreader.dbh._rawQuery(".schema")
+        # print str(rs1)
+        rowCount = rs1[0]['cnt']
+        self.assertTrue(rowCount>1000)
+    ## end
+
+    def testQueryDelegated2(self):
+        rs1 = self.__class__.dsreader.dbh._rawQuery("SELECT count(*) as cnt FROM numres WHERE status='allocated'")
+        # rs1 = self.dsreader.dbh._rawQuery(".schema")
+        # print "allocated %s" % (str(rs1))
+        rowCount = rs1[0]['cnt']
+        self.assertTrue(rowCount>1000)
+    ## end
+
+    def testQueryDelegated3(self):
+        rs1 = self.__class__.dsreader.dbh._rawQuery("SELECT count(*) as cnt FROM numres WHERE status='available'")
+        # rs1 = self.dsreader.dbh._rawQuery(".schema")
+        # print "available %s" % (str(rs1))
+        rowCount = rs1[0]['cnt']
+        self.assertTrue(rowCount>5)
+    ## end
+
+    def testQueryDelegated3(self):
+        rs1 = self.__class__.dsreader.dbh._rawQuery("SELECT count(*) as cnt FROM numres WHERE status='reserved'")
+        # rs1 = self.dsreader.dbh._rawQuery(".schema")
+        # print "reserved %s" % (str(rs1))
+        rowCount = rs1[0]['cnt']
+        self.assertTrue(rowCount>5)
+    ## end
+
+    def testAddPrefixColumn(self):
+        rs = self.__class__.dsreader._add_prefix_column()
+        self.assertTrue(rs, "Error creating prefix column")
+    ## end
+
+    def testAddEquivColumn(self):
+        rs = self.__class__.dsreader._add_equiv_column()
+        self.assertTrue(rs, "Error creating equiv column")
+    ## end
+
+    def testAddNumBeginAndEnd(self):
+        rs = self.__class__.dsreader._add_numeric_columns()
+        self.assertTrue(rs, "Error creating numeric columns")
     ## end
 
 ## end class Test
