8678cd5b2dcd0c2c2a390355c619079b415b65e2
Carlos M. Martinez <carlos@lacnic.net>
2017-04-07 16:44:03 -0300

    fixed typo

--- bin/netdata.py
+++ bin/netdata.py
@@ -25,16 +25,22 @@ prg_date = "2017-04-04"
 
 # set filename
 fname_base = "var/netdata-%s.db" % (date.today())
+dateToday = date.today()
 
 
 def import_delegated_stats(rir, date):
     ## Import delegated stats files
     print "====>> RUNNING: Import delegated stats files"
-    d = dlg.delegatedStats(rir='lacnic', date='latest', db_filename=fname_base)
-    d = dlg.delegatedStats(rir='arin', date='latest', db_filename=fname_base, as_cache=True)
-    d = dlg.delegatedStats(rir='ripencc', date='latest', db_filename=fname_base, as_cache=True)
-    d = dlg.delegatedStats(rir='afrinic', date='latest', db_filename=fname_base, as_cache=True)
-    d = dlg.delegatedStats(rir='apnic', date='latest', db_filename=fname_base, as_cache=True)
+    if rir == "lacnic" or rir == "all":
+        d = dlg.delegatedStats(rir='lacnic', date='latest', db_filename=fname_base)
+    if rir == "arin" or rir == "all":
+        d = dlg.delegatedStats(rir='arin', date='latest', db_filename=fname_base, as_cache=True)
+    if rir == "ripencc" or rir == "all":
+        d = dlg.delegatedStats(rir='ripencc', date='latest', db_filename=fname_base, as_cache=True)
+    if rir == "afrinic" or rir == "all":
+        d = dlg.delegatedStats(rir='afrinic', date='latest', db_filename=fname_base, as_cache=True)
+    if rir == "apnic" or rir == "all":
+        d = dlg.delegatedStats(rir='apnic', date='latest', db_filename=fname_base, as_cache=True)
     print " "
     print "============================================="
 ## END import delegated stats
@@ -68,7 +74,11 @@ def cli(verbose):
 @click.option("--rir", default="all", help="RIR data to import, can be all|<rirname>. Defaults to all")
 @click.option("--date", default="latest", help="Date to import in YYYYMMDD format or 'latest'. Defaults to 'latest'")
 @click.option("--dset", default="all", help="Dataset to import, can be delegated|ripevalapp|riswhois|all. Defaults to all")
-def get(rir, date, dset):
+@click.option("--basename", default="var/netdata", help="Filename base, defaults to var/netdata. Date tag is added automatically")
+def get(rir, date, dset, basename):
+    global fname_base 
+    fname_base = "%s-%s.db" % (basename, dateToday)
+    click.echo("Database filename is %s" % (fname_base))
 
     if dset=="delegated" or dset=="all":
         import_delegated_stats(rir, date)
@@ -78,12 +88,11 @@ def get(rir, date, dset):
 
     if dset == "riswhois" or dset == "all":
         import_riswhois(rir, date)
-
 ## END import tasks
 
 @cli.command()
 def cleanup():
-    click.echo("Doing some cleanup... TBI\n")
+    click.echo("Doing some cleanup... WARN: TBI\n")
     pass
 ## END cleanup
 
@@ -91,7 +100,7 @@ def cleanup():
 if __name__ == "__main__":
     #
     click.echo("Welcome to NETDATA: Importing Internet addressing-related datasets\n")
-    click.echo("  -Author : carlos@lacnic.net")
-    click.echo("  -Version: %s, released %s\n" % (prg_version, prg_date))
+    click.echo("   Author : carlos@lacnic.net")
+    click.echo("   Version: %s, released %s\n" % (prg_version, prg_date))
     cli()
     sys.exit()
