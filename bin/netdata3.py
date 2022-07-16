478e32427a666e28edbb9e72575ed054bcef000e
Carlos Martinez <carlos@cagnazzo.uy>
2022-07-16 13:22:49 -0300

    primer commit en serio de netdata v3, con docs y framework

--- bin/netdata3.py
+++ bin/netdata3.py
@@ -1,8 +1,37 @@
 #!/usr/bin/env python3
+"""
+netdata3, top element of the project.
+=============================================
+
+:author: CarlosM
+:date: July 2022
+
+
+
+"""
 
 # BEGIN
+import click
+
+_VERSION = "3.0.1"
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.option("--dataset", help="Dataset to import. Values are delegated|ris|rpki")
+def load(dataset):
+    print(f"Importing dataset: {dataset}")
+# end import
+
+cli.add_command(load)
 
 if __name__ == "__main__":
     print("Bienvenido a netdada3")
+    print(" ")
+    print("(c) Carlos M. Martinez, julio 2022 @SanPedroDelTimote")
+    print(f"Version: {_VERSION}")
+    cli()
 
 # END
---
+++ lib/lacniclabs/imports/generic.py
@@ -0,0 +1,40 @@
+"""
+imports.generic
+
+:author: Carlos M. Martinez
+:date: July 2022
+
+Generic skeleton for dataset import.
+"""
+
+import petl
+import pytricia
+
+class GenericImport:
+    """
+    GenericImport is an abstract class which serves as an skeleton for other imports.
+    """
+
+    def __init__(self) -> None:
+        pass
+    # end def 
+
+    def load(self) -> None:
+        """
+        Loads data
+        """
+        pass
+    # end def
+
+    def save(self) -> None:
+        """
+        Saves imported data
+        """
+    
+    # end def
+
+# end class
+
+if __name__ == "__main__":
+    print("Not to be run directly")
+    raise Exception("Not to be run directly")
